ad11ec0664a83462e55217f17c26e4e2a8593c21
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-05-17 20:10:36 +0100

    last changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,16 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+
+const productID = getParam("product");
 
 const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productID, dataSource);
+
+product.init();
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
