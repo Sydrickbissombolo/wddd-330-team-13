4bef1261ea6762af0744316e9c7c4147717b7d33
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-05-17 22:04:22 +0100

    Dydnamic product list updated

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,20 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
 import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-
+const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
-const dataSource = new ProductData("tents");
-
 const product = new ProductDetails(productID, dataSource);
-
 product.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  // localStorage.setItem("so-cart", JSON.stringify(cart));
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
