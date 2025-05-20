9b1a724d21586d9e8b1bf1e238598768899c40de
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-05-20 10:07:50 +0100

    fixed issues with loading time

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -46,15 +46,19 @@ export function renderWithTemplate(template, parentElement, data, callback) {
   }
 }
 
-export async function loadTemplate(path) {
+async function loadTemplate(path) {
   const res = await fetch(path);
   const template = await res.text();
   return template;
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTeamplate("../partials/header.html");
-  const headerElement = document.querySelector("#main-header");
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
 
-  renderListWithTemplate(headerTemplate, headerElement);
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
