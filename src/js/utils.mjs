10a6414763bbaa302f117639a0a4c315f7362603
Sydrickbissombolo <newtonbissomblo2@gmail.com>
2025-05-20 09:48:17 +0100

    dynamic header and footer

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -38,3 +38,23 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTeamplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+
+  renderListWithTemplate(headerTemplate, headerElement);
+}
